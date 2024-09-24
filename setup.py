fafcfc89772f2cab04fedbf1da31fb35a4f019b2
abdozmantar <abdozmantar@gmail.com>
2024-09-24 13:18:00 +0300

    Initial commit: created ZipML package for automated machine learning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zipml',
-    version='0.1.2',
+    version='0.1.3',
     description='A simple AutoML tool for small datasets with useful helper functions',
     author='Abdullah OZMANTAR',
     author_email='abdullahozmntr@example.com',
